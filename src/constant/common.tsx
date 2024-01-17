23759c7b778d0436dc47bd97089df8a6a12acb49
OoHuakoO <komsark_@hotmail.co.th>
2024-01-18 01:06:23 +0700

    feat: add common constant

---
+++ src/constant/common.tsx
@@ -0,0 +1,12 @@
+export const STATUS_DOCUMENT = {
+  Draft: 'Draft',
+  Inprogress: 'Inprogress',
+  Done: 'Done',
+  Cancel: 'Cancel',
+};
+
+export const STATUS_ASSET = {
+  New: 'New',
+  Damaged: 'ชำรุด',
+  Repair: 'รอส่งซ่อม',
+};
