d89faf719f62919b4197cd1b1cedf540330aac61
OoHuakoO <komsark_@hotmail.co.th>
2024-01-20 16:32:55 +0700

    fix prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,16 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
-  trailingComma: 'all',
-  endOfLine: 'auto',
-  parser: 'flow',
+    singleQuote: true,
+    printWidth: 80,
+    proseWrap: 'always',
+    tabWidth: 4,
+    requireConfig: false,
+    useTabs: false,
+    trailingComma: 'none',
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    semi: true,
+    arrowParens: 'avoid',
+    bracketSameLine: true,
+    bracketSpacing: true,
+    endOfLine: 'auto'
 };
