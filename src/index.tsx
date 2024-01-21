1dde05266d0a4cebc9f6137ccf7bf7e4c86dbe03
BUNYANUCH SARAK <bunyanuch.sarak@scb.co.th>
2024-01-21 22:53:15 +0700

    Edit ScrollView in setting page

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,15 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
 // import LoginScreen from './screens/login';
-// import SettingScreen from './screens/setting';
-import HomeScreen from './screens/home';
+import SettingScreen from './screens/setting';
+// import HomeScreen from './screens/home';
 const Stack = createStackNavigator();
 function MyStack() {
     return (
         <Stack.Navigator>
             {/* <Stack.Screen name="LoginScreen" component={LoginScreen} /> */}
-            <Stack.Screen name="Home" component={HomeScreen} />
-            {/* <Stack.Screen name="Settings" component={SettingScreen} /> */}
+            {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
+            <Stack.Screen name="Settings" component={SettingScreen} />
             {/* <Stack.Screen name="Login" component={LoginScreen} /> */}
         </Stack.Navigator>
     );
--- src/screens/setting.tsx
+++ src/screens/setting.tsx
@@ -5,8 +5,9 @@ import InputText from '@src/components/core/inputText';
 import StatusTag from '@src/components/core/statusTag';
 import { theme } from '@src/theme';
 import { Navigation } from '@src/typings/navigattion';
-import { ScrollView, StyleSheet, Switch, View } from 'react-native';
+import { ScrollView, StatusBar, StyleSheet, Switch, View } from 'react-native';
 import { Button, Text } from 'react-native-paper';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 interface SettingScreenProps {
     navigation: Navigation;
@@ -17,6 +18,7 @@ type ButtonVisibility = {
 };
 
 const SettingScreen: FC<SettingScreenProps> = () => {
+    //Dialog
     const [visible, setVisible] = React.useState<ButtonVisibility>({});
 
     const _toggleDialog = (name: string) => () =>
@@ -24,6 +26,7 @@ const SettingScreen: FC<SettingScreenProps> = () => {
 
     const _getVisible = (name: string) => !!visible[name];
 
+    //Input
     const [settingValues, setSettingValues] = useState({
         input1: '',
         input2: '',
@@ -33,113 +36,142 @@ const SettingScreen: FC<SettingScreenProps> = () => {
         input6: ''
     });
 
+    const [isEdited, setIsEdited] = useState<boolean>(false);
+
     const handleInputChange = (name, value) => {
         setSettingValues((prevValues) => ({ ...prevValues, [name]: value }));
+        setIsEdited(true);
     };
 
+    //Save Button
     const handleSaveSettings = () => {
         // Add your logic to save the settings (e.g., send to server, store in AsyncStorage)
         console.log('Saving settings:', settingValues);
+        setIsEdited(false);
     };
 
+    //Switch
     const [isEnabled, setIsEnabled] = useState(false);
     const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
 
     return (
-        <ScrollView contentContainerStyle={styles.container}>
-            <View>
-                <Text variant="headlineLarge" style={styles.textHeader}>
-                    Document
-                </Text>
-                <InputText
-                    placeholder="Setting 1"
-                    value={settingValues.input6}
-                    onChangeText={(text) => handleInputChange('input6', text)}
-                />
-            </View>
-            <View>
-                <Text variant="headlineLarge" style={styles.textHeader}>
-                    Odoo Config
-                </Text>
-                <View style={styles.modeSection}>
-                    <View style={styles.statusTag}>
-                        <Text variant="bodyLarge">Mode</Text>
-                        <StatusTag status={'Online'} />
-                    </View>
-                    <View>
-                        <Switch
-                            trackColor={{ false: '#767577', true: '#81b0ff' }}
-                            thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
-                            ios_backgroundColor="#3e3e3e"
-                            onValueChange={toggleSwitch}
-                            value={isEnabled}
-                        />
-                    </View>
+        <SafeAreaView style={styles.container}>
+            <ScrollView style={styles.scrollView}>
+                <View>
+                    <Text variant="headlineLarge" style={styles.textHeader}>
+                        Document
+                    </Text>
+                    <InputText
+                        placeholder="Setting 1"
+                        value={settingValues.input6}
+                        onChangeText={(text) =>
+                            handleInputChange('input6', text)
+                        }
+                    />
                 </View>
-                <InputText
-                    placeholder="Setting 2"
-                    value={settingValues.input6}
-                    onChangeText={(text) => handleInputChange('input6', text)}
+                <View>
+                    <Text variant="headlineLarge" style={styles.textHeader}>
+                        Odoo Config
+                    </Text>
+                    <View style={styles.modeSection}>
+                        <View style={styles.statusTag}>
+                            <Text variant="bodyLarge">Mode</Text>
+                            <StatusTag status={'Online'} />
+                        </View>
+                        <View>
+                            <Switch
+                                trackColor={{
+                                    false: '#767577',
+                                    true: '#81b0ff'
+                                }}
+                                thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+                                ios_backgroundColor="#3e3e3e"
+                                onValueChange={toggleSwitch}
+                                value={isEnabled}
+                            />
+                        </View>
+                    </View>
+                    <InputText
+                        placeholder="Setting 2"
+                        value={settingValues.input6}
+                        onChangeText={(text) =>
+                            handleInputChange('input6', text)
+                        }
+                    />
+                    <InputText
+                        placeholder="Setting 3"
+                        value={settingValues.input6}
+                        onChangeText={(text) =>
+                            handleInputChange('input6', text)
+                        }
+                    />
+                    <InputText
+                        placeholder="Setting 4"
+                        value={settingValues.input6}
+                        onChangeText={(text) =>
+                            handleInputChange('input6', text)
+                        }
+                    />
+                    <InputText
+                        placeholder="Setting 5"
+                        value={settingValues.input6}
+                        onChangeText={(text) =>
+                            handleInputChange('input6', text)
+                        }
+                    />
+                    <InputText
+                        placeholder="Setting 6"
+                        value={settingValues.input6}
+                        onChangeText={(text) =>
+                            handleInputChange('input6', text)
+                        }
+                    />
+                </View>
+                {isEdited && (
+                    <View style={styles.row}>
+                        <Button
+                            mode="contained-tonal"
+                            onPress={_toggleDialog('dialog6')}
+                        >
+                            Close
+                        </Button>
+                        <Button
+                            mode="contained-tonal"
+                            style={styles.button}
+                            onPress={() => {
+                                handleSaveSettings;
+                            }}
+                        >
+                            Save
+                        </Button>
+                    </View>
+                )}
+                <DialogWithIcon
+                    visible={_getVisible('dialog6')}
+                    close={_toggleDialog('dialog6')}
+                    titleText="testt jaa"
+                    contentText="test hiiii"
                 />
-                <InputText
-                    placeholder="Setting 3"
-                    value={settingValues.input6}
-                    onChangeText={(text) => handleInputChange('input6', text)}
-                />
-                <InputText
-                    placeholder="Setting 4"
-                    value={settingValues.input6}
-                    onChangeText={(text) => handleInputChange('input6', text)}
-                />
-                <InputText
-                    placeholder="Setting 5"
-                    value={settingValues.input6}
-                    onChangeText={(text) => handleInputChange('input6', text)}
-                />
-                <InputText
-                    placeholder="Setting 6"
-                    value={settingValues.input6}
-                    onChangeText={(text) => handleInputChange('input6', text)}
-                />
-            </View>
-            <View style={styles.row}>
-                <Button
-                    mode="contained-tonal"
-                    onPress={_toggleDialog('dialog6')}
-                >
-                    Close
-                </Button>
-                <Button
-                    mode="contained-tonal"
-                    style={styles.button}
-                    onPress={() => {
-                        handleSaveSettings;
-                    }}
-                >
-                    Save
-                </Button>
-            </View>
-            <DialogWithIcon
-                visible={_getVisible('dialog6')}
-                close={_toggleDialog('dialog6')}
-                titleText="testt jaa"
-                contentText="test hiiii"
-            />
-        </ScrollView>
+            </ScrollView>
+        </SafeAreaView>
     );
 };
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: StatusBar.currentHeight
+    },
+    scrollView: {
+        // backgroundColor: 'pink',
+        marginHorizontal: 20
+    },
     textHeader: {
         width: '100%',
         color: theme.colors.textPrimary,
         fontWeight: '700',
         textAlign: 'left',
         marginBottom: 15
-    },
-    container: {
-        flex: 1,
-        padding: 16
     },
     modeSection: {
         display: 'flex',
