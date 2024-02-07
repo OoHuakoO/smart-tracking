b47f2a73d01bec400ad272eb2dc627e1b19e237e
BUNYANUCH SARAK <bunyanuch.sarak@scb.co.th>
2024-02-07 20:59:44 +0700

    add document page with document card

---
+++ src/components/views/documentCard.tsx
@@ -0,0 +1,118 @@
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import React, { FC } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Text } from 'react-native-paper';
+interface DocumentScreenProp {
+    documentTitle: string;
+    locationInfo: string;
+    dateInfo: string;
+    documentStatus: string;
+}
+const DocumentCard: FC<DocumentScreenProp> = (props) => {
+    const { documentTitle, locationInfo, dateInfo, documentStatus } = props;
+    let backgroundColor = 'black';
+    let borderColor = 'black';
+    switch (documentStatus) {
+        case 'draft':
+            backgroundColor = '#2E67A6';
+            borderColor = '#2E67A6';
+            break;
+        case 'inprogress':
+            backgroundColor = '#F8A435';
+            borderColor = '#F8A435';
+            break;
+        case 'done':
+            backgroundColor = '#63CA7F';
+            borderColor = '#63CA7F';
+            break;
+        case 'canceled':
+            backgroundColor = '#F0787A';
+            borderColor = '#F0787A';
+            break;
+    }
+
+    return (
+        <View style={[styles.cardContainer, { borderColor }]}>
+            <View style={styles.deleteIconContainer}>
+                {documentStatus === 'draft' ? (
+                    <FontAwesomeIcon icon={faTrash} color="#F0787A" />
+                ) : null}
+            </View>
+            <View style={styles.textContainer}>
+                <Text style={styles.documentTitle}>{documentTitle}</Text>
+                <Text variant="bodyMedium" style={styles.additionalText}>
+                    <Text variant="bodyMedium" style={styles.primitiveText}>
+                        Location:{' '}
+                    </Text>
+                    {locationInfo}
+                </Text>
+                <Text variant="bodyMedium" style={styles.additionalText}>
+                    <Text variant="bodyMedium" style={styles.primitiveText}>
+                        Date:{' '}
+                    </Text>{' '}
+                    {dateInfo}
+                </Text>
+                <View style={[styles.statusContainer, { backgroundColor }]}>
+                    <Text variant="bodyMedium" style={{ color: '#ffff' }}>
+                        {documentStatus}
+                    </Text>
+                </View>
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    cardContainer: {
+        flexDirection: 'row',
+        width: 370,
+        height: 120,
+        backgroundColor: '#EDEDED',
+        alignItems: 'center',
+        paddingVertical: 5,
+        borderRadius: 15,
+        shadowColor: '#000',
+        borderWidth: 2,
+        borderLeftWidth: 8,
+        shadowOffset: {
+            width: 0,
+            height: 1
+        },
+        shadowOpacity: 0.2,
+        shadowRadius: 1.41,
+        elevation: 5
+    },
+    textContainer: {
+        marginLeft: 20
+    },
+    documentTitle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#404040',
+        marginBottom: 5
+    },
+    additionalText: {
+        fontSize: 14,
+        color: '#777'
+    },
+    primitiveText: {
+        color: '#50555C',
+        fontWeight: '600'
+    },
+    statusContainer: {
+        marginTop: 5,
+        alignSelf: 'flex-start',
+        paddingVertical: 2,
+        paddingHorizontal: 12,
+        borderRadius: 20,
+        alignItems: 'flex-start'
+    },
+    deleteIconContainer: {
+        position: 'absolute',
+        right: 20,
+        top: 15
+    }
+});
+
+export default DocumentCard;
--- src/screens/document.tsx
+++ src/screens/document.tsx
@@ -1,15 +1,153 @@
-import SearchDocument from '@src/components/views/searchDocument';
-import React from 'react';
-import { View } from 'react-native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import BackButton from '@src/components/core/backButton';
+import DocumentCard from '@src/components/views/documentCard';
+import SearchButton from '@src/components/views/searchButton';
+import { theme } from '@src/theme';
+import { PrivateStackParamsList } from '@src/typings/navigation';
+import React, { FC } from 'react';
+import { SafeAreaView, ScrollView, StyleSheet, View } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 import { Text } from 'react-native-paper';
+import {
+    heightPercentageToDP as hp,
+    widthPercentageToDP as wp
+} from 'react-native-responsive-screen';
 
-const Document = () => {
+type DocumentScreenProp = NativeStackScreenProps<
+    PrivateStackParamsList,
+    'Document'
+>;
+
+const DocumentScreen: FC<DocumentScreenProp> = (props) => {
+    const { navigation } = props;
     return (
-        <View>
-            <Text variant="displayLarge">Document</Text>
-            <SearchDocument />
-        </View>
+        <SafeAreaView style={styles.container}>
+            <LinearGradient
+                start={{ x: 0, y: 1 }}
+                end={{ x: 1, y: 0 }}
+                colors={['#2C86BF', '#2C86BF', '#8DC4E6']}
+                style={styles.topSectionList}
+            >
+                <View style={styles.backToPrevious}>
+                    <BackButton
+                        handlePress={() => navigation.navigate('Home')}
+                    />
+                </View>
+                <View style={styles.containerText}>
+                    <Text variant="headlineLarge" style={styles.textHeader}>
+                        Document
+                    </Text>
+                    <Text variant="bodyLarge" style={styles.textDescription}>
+                        เอกสารการตรวจนับทั้งหมด
+                    </Text>
+                </View>
+            </LinearGradient>
+            <View style={styles.listSection}>
+                <View style={styles.searchButtonWrap}>
+                    <SearchButton
+                        handlePress={() => navigation.navigate('Home')}
+                    />
+                </View>
+                <ScrollView>
+                    <Text variant="bodyLarge" style={styles.textTotalDocument}>
+                        Total Document : 3
+                    </Text>
+                    <View style={styles.wrapDetailList}>
+                        <DocumentCard
+                            documentTitle={'Document Draft'}
+                            locationInfo={'H0-10th'}
+                            dateInfo={'01/01/2567'}
+                            documentStatus={'draft'}
+                        />
+                        <DocumentCard
+                            documentTitle={'Document 001'}
+                            locationInfo={'H0-11th'}
+                            dateInfo={'01/01/2567'}
+                            documentStatus={'inprogress'}
+                        />
+                        <DocumentCard
+                            documentTitle={'Document 002'}
+                            locationInfo={'H0-12th'}
+                            dateInfo={'01/01/2567'}
+                            documentStatus={'done'}
+                        />
+                        <DocumentCard
+                            documentTitle={'Document 003'}
+                            locationInfo={'H0-12th'}
+                            dateInfo={'01/01/2567'}
+                            documentStatus={'canceled'}
+                        />
+                    </View>
+                </ScrollView>
+            </View>
+        </SafeAreaView>
     );
 };
 
-export default Document;
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    topSectionList: {
+        height: hp('30%'),
+        width: wp('100%'),
+        position: 'absolute',
+        display: 'flex'
+    },
+    backToPrevious: {
+        marginVertical: 25,
+        marginHorizontal: 15,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        alignSelf: 'stretch'
+    },
+    containerText: {
+        marginHorizontal: 20
+    },
+    textHeader: {
+        color: '#FFFFFF',
+        fontWeight: '700',
+        marginBottom: 10
+    },
+    textDescription: {
+        fontFamily: 'Sarabun-Regular',
+        color: '#FFFFFF'
+    },
+    listSection: {
+        flex: 1,
+        height: hp('30%'),
+        width: wp('100%'),
+        backgroundColor: theme.colors.background,
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+        marginTop: '50%',
+        zIndex: 1
+    },
+    wrapDetailList: {
+        display: 'flex',
+        alignItems: 'center',
+        gap: 15,
+        marginTop: 20,
+        marginBottom: 5
+    },
+    searchButtonWrap: {
+        position: 'absolute',
+        right: 25,
+        top: -20
+    },
+    searchButton: {
+        zIndex: 2
+    },
+    textTotalDocument: {
+        marginLeft: 20,
+        marginTop: 20,
+        fontWeight: '700',
+        fontSize: 15
+    },
+    drawer: {
+        width: '80%' // Adjust the width as needed
+    }
+});
+
+export default DocumentScreen;
