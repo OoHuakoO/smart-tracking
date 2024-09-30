f643a81f32b82f581057992e9d6172672ce2a589
OoHuakoO <komsark_@hotmail.co.th>
2024-09-30 16:49:25 +0700

    remove sessionID

--- src/store/auth.tsx
+++ src/store/auth.tsx
@@ -4,7 +4,6 @@ import { atom, RecoilState } from 'recoil';
 export const loginState: RecoilState<LoginState> = atom({
     key: 'Login',
     default: {
-        session_id: '',
         uid: ''
     }
 });
